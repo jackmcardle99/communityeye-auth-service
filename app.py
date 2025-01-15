d8155d4a3f6c643d52a740e813e094c91663b396
Jack <grutto.dev@proton.me>
2025-01-15 12:59:58 +0000

    flyway set up and now working

--- app.py
+++ app.py
@@ -1,18 +1,54 @@
-from flask import Flask, app
-from blueprints.users import users_bp
+from flask import Flask
 from flask_cors import CORS
 import subprocess
-import os
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-app = Flask(__name__)
-CORS(app)
 
-app.register_blueprint(users_bp)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    init_database()
+    return app
 
-@app.before_request
-def initialise_database():
+
+def init_database():
+    try:
+        # Connect to PostgreSQL server
+        conn = psycopg2.connect(dbname='postgres', user='communityeye', password='communityeye', host='localhost')
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = conn.cursor()
+
+        # Check if the database exists
+        cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'communityeye'")
+        exists = cursor.fetchone()
+
+        if not exists:
+            cursor.execute('CREATE DATABASE communityeye')
+            print("Database 'communityeye' created successfully.")
+        else:
+            print("Database 'communityeye' already exists.")
+
+        cursor.close()
+        conn.close()
+
+        # Apply Flyway migrations
+        apply_flyway_migrations()
+
+    except psycopg2.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+
+
+def apply_flyway_migrations():
     flyway_command = ['flyway', '-configFiles=flyway.conf', 'migrate']
-    subprocess.run(flyway_command, check=True)
+    try:
+        subprocess.run(flyway_command, check=True)        
+    except subprocess.CalledProcessError as e:
+        print(f"Flyway migration failed: {e}")
+
+
+# Initialize Flask app
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- blueprints/users.py
+++ blueprints/users.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, app,make_response, request
+# from flask import Blueprint, app,make_response, request
 
 
-users_bp = Blueprint('users_bp', __name__)
+# users_bp = Blueprint('users_bp', __name__)
 
 
-@users_bp.route('/api/v1/register', methods=['POST'])
-def register_user():
-    print('test')
+# @users_bp.route('/api/v1/register', methods=['POST'])
+# def register_user():
+#     print('test')
