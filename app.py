7b52574467f9e633b088cb42ebbcfba514a48d2a
Jack <grutto.dev@proton.me>
2025-02-16 18:49:20 +0000

    a

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from db import init_database
 from blueprints.auth.auth import auth_bp
 
 
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
@@ -11,16 +12,12 @@ def create_app():
     return app
 
 
+
+
+
+# Initialize Flask app
 app = create_app()
 app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    app.run(debug=True)
-=======
     app.run(debug=True, host='0.0.0.0', port=5001)
->>>>>>> Stashed changes
-=======
-    app.run(debug=True, port=5001)
->>>>>>> 171983d814197ec1053804069c85e62175e27c9e
